37a0f2ba644b5a6408685f53adbb4d3f7c6a7b4d
Jeremy Goossen <jeremy.goossen@goctsi.com>
2021-08-23 22:36:55 -0500

    Updates Git lg alias to format logs with authors and dates. Truncates message depending on terminal size.

--- git/git-aliases/set-lg.sh
+++ git/git-aliases/set-lg.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-git config --global alias.lg "log --graph --oneline --all --max-count=35"
-
+git config --global alias.lg '!f() { : git log ; \
+		LOG_LINE_LENGTH=$((`tput cols`-20)); \
+		FORMAT_STRING='"'"'%C(yellow)%h%Creset %C(bold blue)<%an>%Creset %C(auto)%d%Creset %<|('"'"'$LOG_LINE_LENGTH'"'"',trunc) %s %Cgreen(%cr)'"'"'; \
+		git log --graph --abbrev-commit --date=relative --all --max-count=35 --pretty=format:"$FORMAT_STRING" "$@"; \
+	}; f'
