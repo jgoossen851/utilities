8faacc2b1c3b958790e70e4aeb5ab76e502e86c9
Jeremy Goossen <jeremyg995@gmail.com>
2021-08-26 23:54:34 -0500

    Adds the Mozilla Public License Version 2.0 to the project.

--- git/git-aliases/run-all.sh
+++ git/git-aliases/run-all.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2021, Jeremy Goossen jeremyg995@gmail.com
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 for FILE in $SCRIPT_DIR/set-*.sh; do
--- git/git-aliases/set-lg.sh
+++ git/git-aliases/set-lg.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2021, Jeremy Goossen jeremyg995@gmail.com
+
 git config --global alias.lg '!f() { : git log ; \
 		LOG_LINE_LENGTH=$((`tput cols`-20)); \
 		FORMAT_STRING='"'"'%C(yellow)%h%Creset %C(bold blue)<%an>%Creset %C(auto)%d%Creset %<|('"'"'$LOG_LINE_LENGTH'"'"',trunc) %s %Cgreen(%cr)'"'"'; \
 		git log --graph --abbrev-commit --date=relative --all --max-count=35 --pretty=format:"$FORMAT_STRING" "$@"; \
 	}; f'
+
