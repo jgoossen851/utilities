391258a9db44be31638ec815ad20f4ede16df018
Jeremy Goossen <jeremyg995@gmail.com>
2022-04-07 10:35:03 -0500

    Updates 'lg' alias to process additional options before '--all' switch to enable pruning branches with '--exclude' option.

--- git/git-aliases/set-lg.sh
+++ git/git-aliases/set-lg.sh
@@ -4,10 +4,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #
-# Copyright (c) 2021, Jeremy Goossen jeremyg995@gmail.com
+# Copyright (c) 2022, Jeremy Goossen jeremyg995@gmail.com
 
 git config --global alias.lg '!f() { : git log ; \
         LOG_LINE_LENGTH=$((`tput cols`-25)); \
         FORMAT_STRING='"'"'%C(yellow)%h%Creset %C(bold blue)<%an>%Creset %C(auto)%d%Creset %<|('"'"'$LOG_LINE_LENGTH'"'"',trunc) %s %Cgreen(%cr)'"'"'; \
-        git log --graph --abbrev-commit --date=relative --all --max-count=35 --pretty=format:"$FORMAT_STRING" "$@"; \
+        git log --graph --abbrev-commit --date=relative --max-count=35 --pretty=format:"$FORMAT_STRING" "$@" --all; \
     }; f'
