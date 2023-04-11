3774bb82ce4f4ee365d33012135eeb5c285acccf
Jeremy Goossen <jeremyg995@gmail.com>
2023-04-11 08:51:14 -0500

    Add verbose option to the git 'ftch' alias

--- git/git-aliases/set-ftch.sh
+++ git/git-aliases/set-ftch.sh
@@ -4,6 +4,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #
-# Copyright (c) 2021, Jeremy Goossen jeremyg995@gmail.com
+# Copyright (c) 2023, Jeremy Goossen jeremyg995@gmail.com
 
-git config --global alias.ftch "fetch --prune"
+# Set alias name from file name
+ALIAS="$(echo "${0}" | sed -E 's|^.*set-([^/]*).sh$|\1|')"
+
+git config --global alias."${ALIAS}" "fetch --prune --verbose"
--- git/git-aliases/set-ls-ignore.sh
+++ git/git-aliases/set-ls-ignore.sh
@@ -4,9 +4,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #
-# Copyright (c) 2022, Jeremy Goossen jeremyg995@gmail.com
+# Copyright (c) 2023, Jeremy Goossen jeremyg995@gmail.com
 
+# Set alias name from file name
 ALIAS="$(echo "${0}" | sed -E 's|^.*set-([^/]*).sh$|\1|')"
+
 git config --global alias."${ALIAS}" '!f() { : git clean ; \
         git clean --dry-run -dX | sed '"'"'s/Would remove //'"'"' ; \
     }; f'
