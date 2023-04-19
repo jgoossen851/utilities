b5a060d3fb711835de4f7dbfc21fb5a5d116c761
Jeremy Goossen <jeremyg995@gmail.com>
2023-04-19 10:52:06 -0500

    Add an alternate alias for 'git status' to adjust for a command I consistently mistype

--- git/git-aliases/set-st.sh
+++ git/git-aliases/set-st.sh
@@ -4,6 +4,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #
-# Copyright (c) 2021, Jeremy Goossen jeremyg995@gmail.com
+# Copyright (c) 2023, Jeremy Goossen jeremyg995@gmail.com
 
 git config --global alias.st "status"
+
+# Add another alias to correct for a command I consistently mistype
+git config --global alias.stn "status"
