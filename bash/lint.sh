194ca5d79832d9dbd8d874fab6027127c5c879fa
Jeremy Goossen <jeremyg995@gmail.com>
2023-10-12 12:11:07 -0500

    Add copyright statement to linting script

--- bash/lint.sh
+++ bash/lint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2023, Jeremy Goossen jeremyg995@gmail.com
+
 # Run this script in a Git directory to run linter on all files tracked by Git
 
 # Linter will not run textual replacement on any line containing a pattern
