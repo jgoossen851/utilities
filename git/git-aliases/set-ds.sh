dfb357502f0450e4a5195a49f28d08df36ef21d2
Jeremy Goossen <jeremyg995@gmail.com>
2023-10-23 12:19:07 -0500

    Apply new word-splitting to Git alias `in`

--- git/git-aliases/set-ds.sh
+++ git/git-aliases/set-ds.sh
@@ -7,4 +7,7 @@
 # Copyright (c) 2021, Jeremy Goossen jeremyg995@gmail.com
 
 git config --global alias.ds "diff --word-diff --ignore-space-change --cached"
-git config --global alias.in "diff --word-diff --ignore-space-change --cached"
+
+# Divide words on the following punctuation in addition to spaces:
+#   , ; : + = ' " & | -
+git config --global alias.in 'diff --color-words="[^[:space:],;:\\+=\\'\''\\\"&|-]+|[^[:space:]+]" --ignore-space-change --cached'
