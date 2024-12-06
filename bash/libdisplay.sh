b511f68851f4a069b19ab9e7e8fcd450b4449e39
Jeremy Goossen <jeremy.goossen@goctsi.com>
2024-12-06 12:50:39 -0600

    Replace sample output with formatted help text

--- bash/libdisplay.sh
+++ bash/libdisplay.sh
@@ -6,11 +6,23 @@ function display () {
   echo -e "$(awk -f ${SCRIPT_DIR}/libdisplay.awk <<<"${1}")"
 }
 
-
-# ### Testing output
-# display "Test **Bold** __Ita!*lic__ ||Underlined|| ##dim## @@inverse@@ ~~striked~~ {3;text}"
-# display "Test **Bold and __Italic__ ||Under**lined|| ##dim @@i~~nv##erse@@ st**riked~~ tex**t"
-# display "{1;Critical **Error**:} ##This __is__ an ~~error~~.## ||{3;__You__} [4;**should] @@fix@@ it||"
+# ### Sample help text
+# display "Interpret special characters in input as ANSI formatting strings"
+# display "  !**  toggles **bold** text."
+# display "  !##  toggles ##dim## text."
+# display "  !__  toggles __italic__ text."
+# display "  !||  toggles ||underlined|| text."
+# display "  !@@  toggles @@inverse@@ text."
+# display "  !~~  toggles ~~strikethrough~~ text."
+# display "  !{__n__; changes the {1;foreground color}; See below for value of __n__."
+# display "  !}   resets the foreground color to default value"
+# display "  ![__n__; changes the [1;background color]; See below for value of __n__."
+# display "  !]   resets the background color to default value"
+# display "  !!   Escape character: prevents the following character from being interpreted."
+# display "           (Use **!!!!** to output a literal exclaimation mark.)"
+# display "Color specifiers (valid values for __n__)"
+# display "    0: [0;Black]  1: {1;Red}      2: {2;Green}  3: {3;Yellow}"
+# display "    4: [4;Blue]   5: {5;Magenta}  6: {6;Cyan}   7: {7;White}"
 
 ### Previous implementations
 
